82a833d67af723b44c1c4d75e14d15cf42a55614
Coun7ered <erik.bondel@gmx.de>
2024-11-09 19:11:35 +0100

    - improved generation performance

--- src/main/java/net/countered/terrainslabs/worldgen/slabfeature/SlabFeatureLogic.java
+++ src/main/java/net/countered/terrainslabs/worldgen/slabfeature/SlabFeatureLogic.java
@@ -13,6 +13,7 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
@@ -29,7 +30,6 @@ public class SlabFeatureLogic extends Feature<DefaultFeatureConfig> {
     public static final Set<Block> VALID_BLOCKS_FOR_SLAB_PLACEMENT = new HashSet<>();
 
     static {
-
         VALID_BLOCKS_FOR_SLAB_PLACEMENT.add(Blocks.GRASS_BLOCK);
         VALID_BLOCKS_FOR_SLAB_PLACEMENT.add(Blocks.PODZOL);
         VALID_BLOCKS_FOR_SLAB_PLACEMENT.add(Blocks.MYCELIUM);
@@ -64,65 +64,83 @@ public class SlabFeatureLogic extends Feature<DefaultFeatureConfig> {
         VALID_BLOCKS_FOR_SLAB_PLACEMENT.add(Blocks.YELLOW_TERRACOTTA);
         VALID_BLOCKS_FOR_SLAB_PLACEMENT.add(Blocks.WHITE_TERRACOTTA);
     }
-
+    private static final Set<Block> SOIL_SLAB_BLOCKS = Set.of(
+            ModBlocksRegistry.GRASS_SLAB,
+            ModBlocksRegistry.PODZOL_SLAB,
+            ModBlocksRegistry.MYCELIUM_SLAB,
+            ModBlocksRegistry.PATH_SLAB
+    );
     @Override
     public boolean generate(FeatureContext<DefaultFeatureConfig> context){
         if (MyModConfig.enableSlabGeneration) {
             return runLogic(context); // Logic for slab generation
         }
-        return true;
+        return false;
     }
 
     private boolean runLogic(FeatureContext<DefaultFeatureConfig> context) {
         WorldAccess world = context.getWorld();
         BlockPos origin = context.getOrigin();
         ChunkPos chunkPos = new ChunkPos(origin);
+        BlockPos highestBlock = new BlockPos.Mutable(0,0,0);
+        BlockPos.Mutable pos = new BlockPos.Mutable();
 
-        // Iterate through the entire chunk (from bottom to height limit)
-        for (int y = world.getBottomY(); y < world.getHeight(); y++) {
+        // Loop through x and z within the chunk boundaries
+        for (int x = chunkPos.getStartX(); x <= chunkPos.getEndX(); x++) {
+            for (int z = chunkPos.getStartZ(); z <= chunkPos.getEndZ(); z++) {
+                pos.set(x, 0, z);
+                // Ensure the chunk at this position is loaded
+                if (world.isChunkLoaded(chunkPos.x, chunkPos.z)) {
+                    BlockPos topPosition = world.getTopPosition(Heightmap.Type.WORLD_SURFACE_WG, pos);
+                    // Check and update highest block
+                    if (highestBlock.getY() < topPosition.getY()) {
+                        highestBlock = topPosition;
+                    }
+                }
+            }
+        }
+
+        for (int y = world.getBottomY(); y < highestBlock.getY(); y++) {
             for (int x = 0; x < 16; x++) {
                 for (int z = 0; z < 16; z++) {
                     BlockPos currentPos = chunkPos.getBlockPos(x, y, z);
-
+                    BlockPos blockBelowPos = currentPos.down();
+                    BlockPos blockAbovePos = currentPos.up();
+                    BlockState blockBelowState = world.getBlockState(blockBelowPos);
+                    BlockState blockAboveState = world.getBlockState(blockAbovePos);
+                    BlockState currentBlockState = world.getBlockState(currentPos);
+                    BlockState slabState;
                     // Check conditions to place a slab on top of the current block
-                    if (shouldPlaceSlabTop(world, currentPos)) {
+                    if (shouldPlaceSlabTop(world, currentPos, blockBelowPos, blockAboveState, blockBelowState, currentBlockState)) {
                         if (world.getBlockState(currentPos).isOf(Blocks.SNOW)){
-                            if (world.getBlockState(currentPos.down()).isOf(Blocks.GRASS_BLOCK)){
-                                world.setBlockState(currentPos.down(), Blocks.DIRT.getDefaultState(), 0);
+                            if (world.getBlockState(blockBelowPos).isOf(Blocks.GRASS_BLOCK)){
+                                world.setBlockState(blockBelowPos, Blocks.DIRT.getDefaultState(), 0);
                                 world.setBlockState(currentPos, ModBlocksRegistry.GRASS_SLAB.getDefaultState().with(Properties.SNOWY, true), 0);
                             }
                             else {
-                                world.setBlockState(currentPos, ModSlabsMap.getSlabForBlock(world.getBlockState(currentPos.down()).getBlock()).getDefaultState(), 0);
+                                world.setBlockState(currentPos, ModSlabsMap.getSlabForBlock(world.getBlockState(blockBelowPos).getBlock()).getDefaultState(), 0);
                             }
-                            world.setBlockState(currentPos.up(), ModBlocksRegistry.SNOW_ON_TOP.getDefaultState(), 0);
-                            continue;
+                            world.setBlockState(blockAbovePos, ModBlocksRegistry.SNOW_ON_TOP.getDefaultState(), 0);
                         }
-                        BlockState blockBelowState = world.getBlockState(currentPos.down());
+                        else {
+                            blockBelowState = world.getBlockState(blockBelowPos);
 
-                        // Retrieve the slab type based on the block below the current position
-                        BlockState slabState = ModSlabsMap.getSlabForBlock(blockBelowState.getBlock()).getDefaultState();
+                            // Retrieve the slab type based on the block below the current position
+                            slabState = ModSlabsMap.getSlabForBlock(blockBelowState.getBlock()).getDefaultState();
 
-                        // Handle grass slab special case by converting grass to dirt before placing the slab
-                        if (slabState.isOf(ModBlocksRegistry.GRASS_SLAB)
-                                || slabState.isOf(ModBlocksRegistry.PODZOL_SLAB)
-                                || slabState.isOf(ModBlocksRegistry.MYCELIUM_SLAB)
-                                || slabState.isOf(ModBlocksRegistry.PATH_SLAB)) {
-                            world.setBlockState(currentPos.down(), Blocks.DIRT.getDefaultState(), 0);
+                            // Handle grass slab special case by converting grass to dirt before placing the slab
+                            if (SOIL_SLAB_BLOCKS.contains(slabState.getBlock())) {
+                                world.setBlockState(blockBelowPos, Blocks.DIRT.getDefaultState(), 0);
+                            }
+
+                            slabState = updateWaterloggedState(world, currentPos, slabState);
+                            world.setBlockState(currentPos, slabState, 0);
                         }
-
-                        slabState = updateWaterloggedState(world, currentPos, slabState);
-                        world.setBlockState(currentPos, slabState, 0);
-                        continue;
                     }
+                    else if (shouldPlaceSlabOnUnderside(world, currentPos, blockAbovePos, blockBelowPos, currentBlockState, blockBelowState)) {
+                        slabState = ModSlabsMap.getSlabForBlock(currentBlockState.getBlock()).getDefaultState();
 
-
-                    if (shouldPlaceSlabOnUnderside(world, currentPos)) {
-                        BlockState previousBlockState = world.getBlockState(currentPos);
-                        BlockState slabState = ModSlabsMap.getSlabForBlock(previousBlockState.getBlock()).getDefaultState();
-
-                        if (slabState.isOf(ModBlocksRegistry.GRASS_SLAB)
-                                || slabState.isOf(ModBlocksRegistry.PODZOL_SLAB)
-                                || slabState.isOf(ModBlocksRegistry.MYCELIUM_SLAB)) {
+                        if (SOIL_SLAB_BLOCKS.contains(slabState.getBlock())) {
                             slabState = ModBlocksRegistry.DIRT_SLAB.getDefaultState();
                         }
                         slabState = slabState.with(Properties.SLAB_TYPE, SlabType.TOP);
@@ -138,38 +156,33 @@ public class SlabFeatureLogic extends Feature<DefaultFeatureConfig> {
     /**
      * Determines if a slab should be placed at the given position based on world conditions.
      */
-    private boolean shouldPlaceSlabTop(WorldAccess world, BlockPos currentPos) {
-        BlockState blockBelow = world.getBlockState(currentPos.down());
-        BlockState blockAbove = world.getBlockState(currentPos.up());
-        BlockState currentBlockState = world.getBlockState(currentPos);
-
-        if (ModSlabsMap.getSlabForBlock(world.getBlockState(currentPos.down()).getBlock()) == Blocks.AIR
-                || world.getBlockState(currentPos.down()).getBlock() instanceof SlabBlock
+    private boolean shouldPlaceSlabTop(WorldAccess world, BlockPos currentPos, BlockPos blockBelowPos, BlockState blockAboveState, BlockState blockBelow, BlockState currentBlockState) {
+        if (ModSlabsMap.getSlabForBlock(world.getBlockState(blockBelowPos).getBlock()) == Blocks.AIR
+                || world.getBlockState(blockBelowPos).getBlock() instanceof SlabBlock
                 || world.getBlockState(currentPos).isOf(Blocks.POWDER_SNOW)){
             return false;
         }
         // Check that the block below is opaque and not a slab, and the position is air or snow
         if (blockBelow.isOpaque() && !(blockBelow.getBlock() instanceof SlabBlock) && bottomOfMountain(world, currentPos)
                 && (!currentBlockState.isOpaque() || currentBlockState.isOf(Blocks.SNOW) || currentBlockState.isReplaceable())
-                && (blockAbove.isAir() || blockAbove.isOf(Blocks.WATER))) {
+                && (blockAboveState.isAir() || blockAboveState.isOf(Blocks.WATER))) {
 
             // Check neighboring blocks to ensure at least one opaque block is adjacent
             for (Direction direction : Direction.Type.HORIZONTAL) {
-                if (badNextToWaterSlab(world, currentPos, direction) ||
-                        world.getBlockState(currentPos.offset(direction)).isOf(Blocks.ICE) ||
-                        world.getBlockState(currentPos.offset(direction)).isOf(Blocks.LAVA)) {
-                    return false;
-                }
-            }
 
-            for (Direction direction1 : Direction.Type.HORIZONTAL) {
-                BlockPos neighborPos = currentPos.offset(direction1);
+                BlockPos neighborPos = currentPos.offset(direction);
                 BlockState neighborState = world.getBlockState(neighborPos);
                 // Check if a neighboring block is opaque and not a slab
-                if (neighborState.isOpaque() && VALID_BLOCKS_FOR_SLAB_PLACEMENT.contains(neighborState.getBlock())
+                if (neighborState.isOpaque() && VALID_BLOCKS_FOR_SLAB_PLACEMENT.contains(blockBelow.getBlock())
                         && !(neighborState.getBlock() instanceof SlabBlock)
                         && (!world.getBlockState(neighborPos.up()).isOpaque() || world.getBlockState(neighborPos.up()).getBlock() == Blocks.SNOW)
                         && !world.getBlockState(neighborPos).isOf(Blocks.SNOW)) {
+
+                    if (badNextToWaterSlab(world, currentPos, blockBelowPos, Direction.NORTH) || badNextToWaterSlab(world, currentPos, blockBelowPos, Direction.WEST) || badNextToWaterSlab(world, currentPos, blockBelowPos, Direction.SOUTH) || badNextToWaterSlab(world, currentPos, blockBelowPos, Direction.EAST) ||
+                            world.getBlockState(currentPos.offset(Direction.NORTH)).isOf(Blocks.ICE) || world.getBlockState(currentPos.offset(Direction.WEST)).isOf(Blocks.ICE) || world.getBlockState(currentPos.offset(Direction.SOUTH)).isOf(Blocks.ICE) || world.getBlockState(currentPos.offset(Direction.EAST)).isOf(Blocks.ICE) ||
+                            world.getBlockState(currentPos.offset(Direction.NORTH)).isOf(Blocks.LAVA) || world.getBlockState(currentPos.offset(Direction.WEST)).isOf(Blocks.LAVA) || world.getBlockState(currentPos.offset(Direction.SOUTH)).isOf(Blocks.LAVA) || world.getBlockState(currentPos.offset(Direction.EAST)).isOf(Blocks.LAVA)){
+                    return false;
+                    }
                     return true;
                 }
             }
@@ -177,12 +190,9 @@ public class SlabFeatureLogic extends Feature<DefaultFeatureConfig> {
         return false;
     }
 
-    private boolean shouldPlaceSlabOnUnderside(WorldAccess world, BlockPos currentPos) {
-        BlockState currentBlock = world.getBlockState(currentPos);
-        BlockState blockBelow = world.getBlockState(currentPos.down());
-        boolean place = false;
+    private boolean shouldPlaceSlabOnUnderside(WorldAccess world, BlockPos currentPos, BlockPos blockAbovePos, BlockPos blockBelowPos, BlockState currentBlock, BlockState blockBelow) {
 
-        if (ModSlabsMap.getSlabForBlock(world.getBlockState(currentPos.up()).getBlock()) == Blocks.AIR || world.getBlockState(currentPos.up()).getBlock() instanceof SlabBlock || nextToGlowLichen(world, currentPos)){
+        if (ModSlabsMap.getSlabForBlock(world.getBlockState(blockAbovePos).getBlock()) == Blocks.AIR || world.getBlockState(blockAbovePos).getBlock() instanceof SlabBlock){
             return false;
         }
         // Check that the block above is a valid block for slab placement and that the current block is air or water
@@ -190,21 +200,26 @@ public class SlabFeatureLogic extends Feature<DefaultFeatureConfig> {
                 && (blockBelow.isAir() || blockBelow.getBlock() == Blocks.WATER)
                 && currentBlock.isOpaque()) {
 
+            for (Direction direction1 : Direction.Type.HORIZONTAL){
+                BlockPos neighborPos = currentPos.offset(direction1);
+                BlockState neighborState = world.getBlockState(neighborPos);
+                if (badNextToWaterSlab(world, currentPos, blockBelowPos, direction1) || nextToGlowLichen(world, currentPos, direction1) || neighborState.isOf(Blocks.LAVA)) {
+                    return false;
+                }
+            }
+
             // Check neighboring blocks to ensure at least one horizontal neighbor is air or water
             for (Direction direction : Direction.Type.HORIZONTAL) {
                 BlockPos neighborPos = currentPos.offset(direction);
                 BlockState neighborState = world.getBlockState(neighborPos);
-                if (badNextToWaterSlab(world, currentPos, direction)) {
-                    return false;
-                }
 
                 // If at least one horizontal neighbor is air or water, mark this position for slab placement
-                if ((neighborState.isAir() || neighborState.getBlock() == Blocks.WATER) && !neighborState.isOf(Blocks.LAVA)) {
-                    place = true;
+                if ((neighborState.isAir() || neighborState.getBlock() == Blocks.WATER)) {
+                    return true;
                 }
             }
         }
-        return place;
+        return false;
     }
 
 
@@ -227,9 +242,9 @@ public class SlabFeatureLogic extends Feature<DefaultFeatureConfig> {
         return slabState;
     }
 
-    private Boolean badNextToWaterSlab(WorldAccess world, BlockPos currentPos, Direction direction) {
+    private Boolean badNextToWaterSlab(WorldAccess world, BlockPos currentPos, BlockPos blockBelowPos, Direction direction) {
         if (world.getBlockState(currentPos.offset(direction)).isOf(Blocks.WATER) &&
-                (world.getBlockState(currentPos.down()).isOf(Blocks.AIR) || world.getBlockState(currentPos.offset(direction.getOpposite())).getBlock() == Blocks.AIR)) {
+                (world.getBlockState(blockBelowPos).isOf(Blocks.AIR) || world.getBlockState(currentPos.offset(direction.getOpposite())).getBlock() == Blocks.AIR)) {
             return true;
         }
         return false;
@@ -248,11 +263,10 @@ public class SlabFeatureLogic extends Feature<DefaultFeatureConfig> {
         return false;
     }
 
-    private boolean nextToGlowLichen(WorldAccess world, BlockPos currentPos) {
-        for (Direction direction : Direction.Type.HORIZONTAL) {
-            if (world.getBlockState(currentPos.offset(direction)).isOf(Blocks.GLOW_LICHEN)){
-                return true;
-            }
+    private boolean nextToGlowLichen(WorldAccess world, BlockPos currentPos, Direction direction) {
+
+        if (world.getBlockState(currentPos.offset(direction)).isOf(Blocks.GLOW_LICHEN)){
+            return true;
         }
         return false;
     }
